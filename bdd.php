c0934abfb6c4c584d54ada9508f2a11bc9d5e748
rasho22 <ab_rasho@yahoo.fr>
2016-09-27 17:14:13 +0200

    POO

--- bdd.php
+++ bdd.php
@@ -15,19 +15,69 @@
 		$connexion = new PDO("mysql:host=$serveur;dbname=test2",$usr,$psw);
 		$connexion->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-		$insertion= "INSERT INTO visiteur (nom, prenom, mail)  VALUES('RASHO', 'Abdul', 'ab_rasho@yahoo.fr'),
-		('SAGE', 'Jean', 'abffa@yahoo.fr')";
-
-		$connexion->exec($insertion);
-		
-		echo ' table visiteur créé!';
-	}
 
 
-	catch (Exception $e){
+
+
+
+		}
+		catch (Exception $e){
 		echo 'Echec de la connection : '.$e->getMessage();
 
 	} 
+
+	/*
+	modifier et suprimer des données:
+	// $suprim="DROP TABLE visiteur"; //suprimer un tableau
+
+		//$suprim="ALTER TABLE visiteur DROP sexe"; //suprimer colone sexe
+		//$modif="UPDATE visiteur SET age=12 WHERE id=1"; //pour modifier l'élément avec id =1
+		$requete=$connexion->prepare($suprim);
+		$requete->execute(); 
+		echo "colone sese suprimé";
+
+	$suprim="DELETE FROM visiteur WHERE id=5";   //suprimer un élément
+		//$modif="UPDATE visiteur SET age=12 WHERE id=1"; //pour modifier l'élément avec id =1
+		$requete=$connexion->prepare($suprim);
+		$requete->execute(); 
+		echo "info mis à jour";
+
+/* sélectionner un élement dans bd:
+		
+		$requete= $connexion->prepare(" SELECT prenom FROM visiteur WHERE sexe='H'");
+		$requete->execute(); 
+		$resultat= $requete->fetchall();
+
+		echo "<pre>";
+			print_r($resultat);
+		echo "</pre>";
+
+
+
+		$requete= $connexion->prepare(" SELECT prenom FROM visiteur WHERE sexe='H'");
+		$requete->execute(); 
+		$resultat= $requete->fetchall();
+
+		echo "<pre>";
+			print_r($resultat);
+		echo "</pre>";*/
+
+
+
+		/* ajouter une colne dans un tableau
+		$requete="ALTER TABLE visiteur ADD age INT(100)";
+		$connexion->exec($requete);*/
+/*
+	sélectioner toutes les données dans le tableau:
+
+		$requete= $connexion->prepare(" SELECT * FROM visiteur");
+		$requete->execute(); 
+		$resultat= $requete->fetchall();
+
+		echo "<pre>";
+			print_r($resultat);
+		echo "</pre>";*/
+	
  	
 	?>
 
@@ -99,6 +149,57 @@
 
 	} 
 
+
+ajouter des données dans un tableau
+$serveur = "localhost";
+		$usr = "Abdul";
+ 		$psw = "facesimplon";
+
+ 		try{
+
+		$connexion = new PDO("mysql:host=$serveur;dbname=test2",$usr,$psw);
+		$connexion->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$insertion= "INSERT INTO visiteur (nom, prenom, mail)  VALUES('RASHO', 'Abdul', 'ab_rasho@yahoo.fr'),
+		('SAGE', 'Jean', 'abffa@yahoo.fr')";
+
+		$connexion->exec($insertion);
+		
+		echo ' table visiteur créé!';
+	}
+
+
+	catch (Exception $e){
+		echo 'Echec de la connection : '.$e->getMessage();
+
+	} 
+
+	ajouter des données sécurisées:
+	try{
+
+		$connexion = new PDO("mysql:host=$serveur;dbname=test2",$usr,$psw);
+		$connexion->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$requete= $connexion->prepare(
+			"INSERT INTO visiteur (nom, prenom, mail) 
+			VALUES (:nom, :prenom, :mail)"
+			);
+
+		$requete->bindParam(':nom', $nom);
+		$requete->bindParam(':prenom', $prenom);
+		$requete->bindParam(':mail', $mail);
+
+		$nom="toto";
+		$prenom="titi";
+		$mail="toto@yahoo.fr";
+		$requete->execute();
+
+		echo 'vous avez ajouter toto';
+	}
+		catch (Exception $e){
+		echo 'Echec de la connection : '.$e->getMessage();
+
+	}
 	-->
 </body>
 </html>
